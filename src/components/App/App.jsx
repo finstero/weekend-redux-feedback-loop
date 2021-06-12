a1317f1b66ff64ed70b137900d85c650fff2aa16
Olivia Finster <olivia.finster@gmail.com>
2021-06-12 11:41:56 -0500

    more install. added dom routes.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,15 +2,42 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+// router for different pages
+import { Route, HashRouter as Router } from "react-router-dom";
+
+// componenets
+
+// material ui
+import Button from '@material-ui/core/Button';
+
+
 function App() {
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+        <Route path='/' exact>
+          <h2>Ready?</h2>
+          <Button variant="outlined">Start</Button>
+        </Route>
+        <Route path='/feeling'>
+          <h2>How are you feeling today?</h2>
+        </Route>
+        <Route path='/understanding'>
+          <h2>How well are you understanding the content?</h2>
+        </Route>
+        <Route path='/supported'>
+          <h2>How well are you being supported?</h2>
+        </Route>
+        <Route path='/comments'>
+          <h2>Any comments?</h2>
+        </Route>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import logger from 'redux-logger';
 
 import './index.css';
 import App from './components/App/App';
