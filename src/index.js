775fba1a1e00bc696e802c1d3a5d8566a760cf21
Olivia Finster <olivia.finster@gmail.com>
2021-06-12 11:21:42 -0500

    imported dependencies. index.js barebones.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(logger)
+)
+
+
+ReactDOM.render(
+    <Provider store={store}>
+<App />
+</Provider>,
+ document.getElementById('root'));
 registerServiceWorker();
