6b32e6748f12076615c856ee5181038630d5d118
Olivia Finster <olivia.finster@gmail.com>
2021-06-13 13:49:42 -0500

    delete route partially made on client.

--- src/components/AdminItem/AdminItem.js
+++ src/components/AdminItem/AdminItem.js
@@ -3,13 +3,31 @@ import { useSelector } from 'react-redux';
 // material ui
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function AdminItem() {
 
-// grabbing all feedback from database stored in allFeedback reducer
-  const allFeedback = useSelector(store => store.allFeedback);
+    // grabbing all feedback from database stored in allFeedback reducer
+    const allFeedback = useSelector(store => store.allFeedback);
 
-  // displays all feedback properties from all feedback currently in db/reducer
+    const handleDelete = () => {
+        console.log('delete');
+        axios.delete({
+            method: 'DELETE',
+            url: '/feedback'
+        })
+        .then(response => {
+            console.log('deleted feedback');
+            // delete from reducer and/or reload? do i need to reload? probs
+        })
+        .catch(err => {
+            console.log('error in delete ', err);
+        })
+    }
+
+
+    // displays all feedback properties from all feedback currently in db/reducer
     return (
         <>
             {allFeedback.map((feedback, i) => (
@@ -19,7 +37,9 @@ function AdminItem() {
                     <TableCell>{feedback.support}</TableCell>
                     <TableCell>{feedback.comments}</TableCell>
                     <TableCell>{feedback.date}</TableCell>
-                    <TableCell></TableCell>
+                    <TableCell><IconButton onClick={handleDelete} aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton></TableCell>
                 </TableRow>
             ))}
         </>
